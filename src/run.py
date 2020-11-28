23acafe898ed533004a5792e7b8ea73193214bda
mirayr <mirayr.raul@gmail.com>
2020-11-28 02:24:14 -0300

    Implement a method with receive a image in rout /rotate, then rotate and save into server.

--- src/run.py
+++ src/run.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from PIL import Image
+import PIL
 
 app = Flask(__name__)
+
+# Envia a imagem para um metodo no servidor
+# Chamar essa função através do cmd curl -F "file=@mirayr.jpg" http://localhost:5000/rotate
+@app.route("/rotate", methods=["POST"])
+def dump_image():
+    img = Image.open(request.files['file'])
+    img = img.rotate(45)
+    img.save("/home/raul/PycharmProjects/rest_python/src/temp_image", "JPEG")
+    return 'Success! '
 
 
 @app.route("/")
