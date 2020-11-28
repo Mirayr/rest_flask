68659e548a4c5132a6347111f2877ffdb9f866e2
mirayr <mirayr.raul@gmail.com>
2020-11-28 01:28:35 -0300

    Change to return a page HTML.

--- src/run.py
+++ src/run.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return jsonify({"message": "Hello!"})
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
